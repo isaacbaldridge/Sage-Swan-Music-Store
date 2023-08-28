cd82b08ee093ec3c401cf8bc4a17f188f2671b30
jellio26 <jasminelelliott@icloud.com>
2023-08-28 13:02:02 -0400

    homepage

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,7 +4,9 @@ import Register from './components/Register';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Nav from './components/Nav';
+import Cart from './components/Cart';
 import {Routes, Route} from 'react-router-dom';
+import SingleProduct from './components/SingleProduct';
 
 function App() {
 const [token, setToken] =  useState('');
@@ -16,6 +18,8 @@ const [token, setToken] =  useState('');
        <Route path="/Login" element={<Login setToken={setToken} />}>Login</Route>
        <Route path="/Register" element={<Register setToken={setToken} />}>Register</Route>
        <Route path="/Profile" element={<Profile token={token} />}>Profile</Route>
+       <Route path="/Cart" element={<Cart/>}>Cart</Route>
+       <Route path="/:id" element={<SingleProduct/>}>SingleProduct</Route>
        </Routes>
     </>
   );
---
+++ src/client/components/SingleProduct.jsx
@@ -0,0 +1,40 @@
+import { useState,useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {Link} from 'react-router-dom';
+
+export default function SingleProduct() {
+    const [singleProduct, setSingleProduct] = useState ({});
+    const {id} = useParams();
+    useEffect(()=>{
+        async function fetchData(){
+            const response = await fetch(`http://localhost:3000/api/products/${id}`)
+            const data = await response.json();
+            console.log(data)
+            setSingleProduct(data)
+        }
+
+        fetchData();
+
+    },[])
+
+
+
+
+    return(
+        <>
+        <div className='SingleProduct'>
+            <h1>Product Details!</h1>
+        
+            <div key = {singleProduct.id}>
+            <h3>{singleProduct.name}</h3> 
+            <p>{singleProduct.description}</p> 
+            <p>{singleProduct.price}</p>
+            <p><img src={singleProduct.image}/></p>
+            </div>
+            <Link to ='/'><button>Go Back</button></Link>
+            <Link to ='/Cart'><button>Add To Cart</button></Link>
+            </div>
+
+        </>)
+
+}
